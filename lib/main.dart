1c3a7b319a33d64931501588c37d6b7a1e260a65
Korlii <adriancurley9@gmail.com>
2026-02-09 02:42:10 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
-import 'screens/splash.dart'; // Import the splash screen we just made
-
+import 'package:google_fonts/google_fonts.dart'; // Add this import
+import 'screens/splash.dart'; 
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SplashScreen(),
-  ));
+  runApp(const CurioStoreApp());
+}
+
+class CurioStoreApp extends StatelessWidget {
+  const CurioStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CurioStore',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        // This line applies DM Sans to every Text widget in your app automatically
+        textTheme: GoogleFonts.dmSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: const SplashScreen(),
+    );
+  }
 }
